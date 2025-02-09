8375758afd5cb0710458b5d04814b8e0e6504706
nick doyle <109041867+ndoyle09@users.noreply.github.com>
2025-02-08 19:17:54 -0500

    Updates

--- js/script.js
+++
@@ -1,7 +0,0 @@
-import EmblaCarousel from 'embla-carousel';
-
-const emblaNode = document.querySelector('.embla')
-const options = { loop: false }
-const emblaApi = EmblaCarousel(emblaNode, options)
-
-console.log(emblaApi.slideNodes()) // Access API
